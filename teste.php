9c79855795d62cc8317ffa472f9fa121f1de206b
sydrack <sydrack@edu.univali.br>
2017-04-02 19:28:16 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -3,6 +3,8 @@ require_once 'biblioteca.php';
 echo "Ola";
 $db = pg_connect (DATABASE);
         $consulta = pg_query($db, "SELECT * FROM usuario where id=1");
-        echo $consulta;
+         while ($row = pg_fetch_assoc($consulta)) {
+            echo $row['login'] $row['senha'];
+        }
         
 ?>
